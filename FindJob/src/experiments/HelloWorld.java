3d3349bf3452459e057bfeb1ca20835fca2ab775
Romil M <romil.bm@gmail.com>
2013-12-19 02:17:01 -0600

    check if strings are isometric

---
+++ FindJob/src/experiments/HelloWorld.java
@@ -0,0 +1,10 @@
+package experiments;
+
+/**
+ * Created by romilbm on 12/19/13.
+ */
+public class HelloWorld {
+    public static void main(String args[]){
+        System.out.println("Testing Isometric:"+ StringOp.isIsometric("foo", "app"));
+    }
+}
